99a441fb6dc10c26fbfeefe07dddd9daa7483ea1
Will Drougas <wdrougas@gmail.com>
2019-10-16 18:19:26 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,5 +18,20 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  
+  i = 0
+  totals = {}
+  
+  while i < nds.length do
+    directors_name = nds[i][:name]
+    totals[directors_name] = 0
+    movie_index = 0
+    
+  while movie_index < nds[i][:movies].length do
+    totals[directors_name] += nds[i][:movies][movie_index][:worldwide_gross]
+    movie_index += 1
+end
+i += 1
+end
+totals
 end
